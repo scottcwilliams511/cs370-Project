0802c18d75c772ddc6a262a826f2f14323fcecbe
Brittney Ryn <bryn-2013@hotmail.com>
2016-04-12 17:55:44 -0700

    more fragments

---
+++ app/src/main/java/com/app/agile_overlords/moveandgroove/Fragments/WeightFragment.java
@@ -0,0 +1,9 @@
+package com.app.agile_overlords.moveandgroove.Fragments;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by brittneyryn on 4/12/16.
+ */
+public class WeightFragment extends Fragment {
+}
