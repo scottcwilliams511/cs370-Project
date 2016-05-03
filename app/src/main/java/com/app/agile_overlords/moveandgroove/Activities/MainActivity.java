c76bd09424706e3eb2fe779ce0c00a59e93f1016
Brittney Ryn <bryn-2013@hotmail.com>
2016-05-03 15:34:31 -0700

    adding

--- app/src/main/java/com/app/agile_overlords/moveandgroove/Activities/MainActivity.java
+++ app/src/main/java/com/app/agile_overlords/moveandgroove/Activities/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 import android.view.MenuItem;
 
 import com.app.agile_overlords.moveandgroove.Fragments.MainFragment;
@@ -15,7 +16,7 @@ import com.app.agile_overlords.moveandgroove.R;
 /**
  * Created by brittneyryn on 4/8/16.
  */
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends BaseActivity {
 
     private MainFragment mainFragment;
     private UserFragment userFragment;
@@ -26,7 +27,14 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity);
+        super.onCreateDrawer();
         setContentView(R.layout.activity_main);
+        toolbar = (Toolbar) findViewById(R.id.toolbar); // Attaching the layout to the toolbar object
+        setSupportActionBar(toolbar);
+
+
         mainFragment = MainFragment.newInstance();
         getSupportFragmentManager().beginTransaction()
                 .add(R.id.container, mainFragment)
@@ -56,6 +64,31 @@ public class MainActivity extends AppCompatActivity {
 */
     // `onPostCreate` called when activity start-up is complete after `onStart()`
     // NOTE! Make sure to override the method with only a single `Bundle` argument
+   @Override
+   public boolean onCreateOptionsMenu(Menu menu) {
+       // Inflate the menu; this adds items to the action bar if it is present.
+       getMenuInflater().inflate(R.menu.menu_main, menu);
+       return true;
+   }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+
+
     @Override
     protected void onPostCreate(Bundle savedInstanceState) {
         super.onPostCreate(savedInstanceState);
